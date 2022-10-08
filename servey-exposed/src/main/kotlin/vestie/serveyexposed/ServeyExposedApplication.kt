ca0e4983fdc16aeb2aeb603e00d8971d3ce1b0db
GwakMirae <alfo9490@naver.com>
2022-10-08 16:06:19 +0900

    project setting

---
+++ servey-exposed/src/main/kotlin/vestie/serveyexposed/ServeyExposedApplication.kt
@@ -0,0 +1,11 @@
+package vestie.serveyexposed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServeyExposedApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServeyExposedApplication>(*args)
+}
---
+++ servey-exposed/src/test/kotlin/vestie/serveyexposed/ServeyExposedApplicationTests.kt
@@ -0,0 +1,13 @@
+package vestie.serveyexposed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServeyExposedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
